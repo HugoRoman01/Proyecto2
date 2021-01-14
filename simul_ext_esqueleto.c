98810f44d68dc2df977b65e78a8d7a10d5474ef7
HugoRoman01 <56384232+HugoRoman01@users.noreply.github.com>
2021-01-14 21:23:15 +0000

    ACTUALIZACION

--- simul_ext_esqueleto.c
+++ simul_ext_esqueleto.c
@@ -31,10 +31,10 @@ int main()
  	{
 	 
  
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
+	 char comando[LONGITUD_COMANDO];
+	 char orden[LONGITUD_COMANDO];
+	 char argumento1[LONGITUD_COMANDO];
+	 char argumento2[LONGITUD_COMANDO];
 	 
 	 int i,j;
 	 unsigned long int m;
@@ -53,6 +53,11 @@ int main()
      
      
      fent = fopen("particion.bin","r+b");
+     if ((fent = fopen("particion.bin","r+b")) == NULL){
+     	
+     	printf ( " Error en la apertura ");
+     	
+	 }
      fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
      
      
@@ -67,27 +72,219 @@ int main()
 		 do {
 		 printf (">> ");
 		 fflush(stdin);
-		 fgets(*comando, LONGITUD_COMANDO, stdin);
-		 } while (ComprobarComando(*comando,*orden,*argumento1,*argumento2) !=0);
-	     if (strcmp(*orden,"dir")==0) {
-            Directorio(&*directorio,&ext_blq_inodos);
-            continue;
+		 fgets(comando, LONGITUD_COMANDO, stdin);
+		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
+	    
+		 if (strcmp(orden,"info")==0) {
+		 	
+		 LeeSuperBloque(&ext_superblock);	 	
+		 	
+		 }
+		 
+		  if (strcmp(orden,"bytemaps")==0){
+		  	
+		  	Printbytemaps(&ext_bytemaps);
+		 
+		 }
+		
+		 if (strcmp(orden,"dir")==0) {
+            Directorio(directorio,&ext_blq_inodos);
             }
-         
+            
+         if(strcmp(orden,"rename")==0){
+         	switch(Renombrar(directorio,&ext_blq_inodos,argumento1,argumento2)){
+         	
+			 case -1: printf("ERROR: el fichero %s ya existe\n",argumento2);
+			 	break;
+			 
+			 case 0 : 
+			 	printf("ERROR: el fichero %s no encontrado\n",argumento1);	
+			 	break;
+			
+			 case 1: grabardatos =1;
+			 break;
+         		
+         	
+			 }
+		 }
+         /*
          // Escritura de metadatos en comandos rename, remove, copy     
          Grabarinodosydirectorio(&*directorio,&ext_blq_inodos,fent);
          GrabarByteMaps(&ext_bytemaps,fent);
          GrabarSuperBloque(&ext_superblock,fent);
-         if (grabardatos)
+         if (grabardatos){
+		 
            GrabarDatos(&*memdatos,fent);
          grabardatos = 0;
+     }
+     	if (strcmp(*orden,"rename")==0){
+		  	
+		 
+		 }
+		 
+		 if (strcmp(*orden,"imprimir")==0){
+		  	
+		 
+		 }
+		 
+		 if (strcmp(*orden,"remove")==0){
+		  	
+		 
+		 }
+		 
+		 if (strcmp(*orden,"copy")==0){
+		  	
+		 
+		 }
+		
+     
+     
          //Si el comando es salir se habrÃ¡n escrito todos los metadatos
          //faltan los datos y cerrar
          if (strcmp(*orden,"salir")==0){
             GrabarDatos(&*memdatos,fent);
             fclose(fent);
             return 0;
-         }
+         }*/
+ 
+        }
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////		 
+		 
      }
- }
+ 
+void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup){
+	
+	printf("El bloque tiene %d bytes", psup->s_block_size);
+    printf("\nInodos particion = %d", psup->s_inodes_count);
+    printf("\ninodos libres = %d", psup->s_free_inodes_count);
+    printf("\nBloques particion = %d", psup->s_blocks_count);
+    printf("\nBloques libres = %d", psup->s_free_blocks_count);
+    printf("\nPrimer bloque de datos = %d\n\n", psup->s_first_data_block);
+
+}
+
+void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps){
+    printf("Inodos :");
+    int i, j;
+    for(i = 0;i< MAX_INODOS; i++){
+        printf("%d ", ext_bytemaps ->bmap_inodos[i]);
+    }
+    printf("\nBloques [0-25] :");
+    for(j=0; j<25; j++){
+        printf("%d ", ext_bytemaps ->bmap_bloques[j]);
+    }
+    printf("\n");
+
+}
+
+
+void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos){
+	
+	int i,j;
+	
+	for(i=1;MAX_FICHEROS;i++){
+		if((directorio+i)->dir_inodo!=NULL_INODO){
+			printf("%s\ttamaño: %d\tinodo: %d\tbloques:",(directorio+i)->dir_nfich, inodos->blq_inodos[(directorio+i)->dir_inodo].size_fichero, (directorio+i)->dir_inodo);
+			for(j=0; inodos ->blq_inodos[(directorio+i)->dir_inodo].i_nbloque[j]!=NULL_INODO;j++){
+				
+					printf("%d", inodos->blq_inodos[(directorio+i)->dir_inodo].i_nbloque[j]);
+			}
+			printf("\n");
+		}
+	
+	}
+
+}
+
+
+int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombreantiguo, char *nombrenuevo){
+	
+	short unsigned int comprobar=0;
+	short unsigned int wardI;
+	int i;
+	
+	for( i=1; ((directorio+i)->dir_inodo)!=NULL_INODO;i++){
+		
+		if((strcmp(nombreantiguo,(directorio+i)->dir_nfich)==0)){
+			
+			comprobar++;
+			wardI=i;
+			
+		
+		}
+		if(strcmp(nombrenuevo,(directorio+i)->dir_nfich)==0){
+			return -1;
+		}
+	}
+	
+	if(comprobar=1){
+		strcpy((directorio+wardI)->dir_nfich,nombrenuevo);
+	}
+	return comprobar;
+	
+	
+}
+
+int ComprobarComando( char *strcomando, char *orden, char *argumento1, char *argumento2){
+
+	//VACIAMOS LOS COMANDOS
+	
+	strcpy(orden,"");
+	strcpy(argumento1,"");
+	strcpy(argumento2,"");
+	
+	char temp[strlen(strcomando)];
+	strncpy(temp, strcomando, strlen(strcomando)-1);
+	temp[strlen(strcomando)-1]= '\0';
+	char *token = strtok(temp," ");
+	
+	//DIVIDIR EL COMANDO
+	
+	if(token != NULL){
+		int i;
+		for(i =0; token != NULL; i++){
+			
+			switch(i){
+			
+			
+				case 0:
+						strcpy(orden, token);
+						token = strtok(NULL," ");
+						break;
+						
+				case 1:
+						strcpy(argumento1, token);
+						token = strtok(NULL," ");
+						break;
+				
+				case 2:
+						strcpy(argumento2, token);
+						token = strtok(NULL," ");
+						break;
+				
+				default:
+						return 1;
+												
+		}
+	}
+	
+}else{
+		return 1;
+}
+		
+		if(strcmp(orden,"info")==0){
+			return 0;
+		}else if(strcmp(orden,"bytemaps")==0){
+			return 0;
+		}else if(strcmp(orden,"dir")==0){
+			return 0;
+		}else if(strcmp(orden,"rename")==0){
+			return !(strlen(argumento1)>0 && strlen(argumento2)>0);
+		}
+
+
+}
+
+
 
